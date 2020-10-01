b9e8bcbd236e2024a15839165ad4661f5848ebf5
Krystyna <krystyna.voroniuk@nure.ua>
2020-10-01 16:47:57 +0300

    1

---
+++ 3_lesson/date.js
@@ -0,0 +1,21 @@
+let date1 = new Date('2020-09-07T01:15');
+let date2 = new Date('2020-09-21T05:15');
+
+
+function differenceday(){
+    let count = 0;
+
+    while(date1.getTime() < date2.getTime()){
+        if(date1.getDay() !== 0 && date1.getDay() !== 6){
+            count++;
+        }
+        date1.setDate(date1.getDate() + 1) ;
+
+
+    }
+    return count;
+
+
+
+}
+console.log(differenceday(date1, date2));
