bff03950924ab50636f6e6f7885bec39543a0518
Krystyna <krystyna.voroniuk@nure.ua>
2020-10-14 12:33:45 +0300

    5

--- 4_lesson/Task2/div.js
+++ 4_lesson/Task2/div.js
@@ -4,7 +4,7 @@ function generateColor() {
 }
 
 function getRandomPlace(min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
     rand = Math.round(rand);
     return rand;
 }
--- 4_lesson/Task3/drag-n-drop.js
+++ 4_lesson/Task3/drag-n-drop.js
@@ -1,19 +1,16 @@
 let my_div = document.querySelector('.block');
-my_div.onmousedown = function(event) { // (1) отследить нажатие
+my_div.onmousedown = function(event) { 
 
-    // (2) подготовить к перемещению:
-    // разместить поверх остального содержимого и в абсолютных координатах
     my_div.className = "block";
     my_div.style.position = 'absolute';
     my_div.style.zIndex = 1000;
-    // переместим в body, чтобы мяч был точно не внутри position:relative
+
     document.body.append(my_div);
-    // и установим абсолютно спозиционированный мяч под курсор
+
   
     moveAt(event.pageX, event.pageY);
   
-    // передвинуть мяч под координаты курсора
-    // и сдвинуть на половину ширины/высоты для центрирования
+
     function moveAt(pageX, pageY) {
       my_div.style.left = pageX - my_div.offsetWidth / 2 + 'px';
       my_div.style.top = pageY - my_div.offsetHeight / 2 + 'px';
@@ -22,11 +19,10 @@ my_div.onmousedown = function(event) { // (1) отследить нажатие
     function onMouseMove(event) {
       moveAt(event.pageX, event.pageY);
     }
-  
-    // (3) перемещать по экрану
+ 
     document.addEventListener('mousemove', onMouseMove);
   
-    // (4) положить мяч, удалить более ненужные обработчики событий
+
     my_div.onmouseup = function() {
       document.removeEventListener('mousemove', onMouseMove);
       my_div.onmouseup = null;
